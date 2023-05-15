f6a2004896c124a648e7f0f047bf107a34c9a211
mustafa <m.gonulgul49@gmail.com>
2023-05-15 23:27:11 +0300

    v1 15 05 2023

--- src/components/common/contact/contact.js
+++ src/components/common/contact/contact.js
@@ -76,49 +76,7 @@ const Contact = () => {
         ></iframe>
       </div>
       <Footer/>
-      {/* <div className="footer">
-        <Row>
-          <Col lg={3}>
-            <h2> KURUMSAL</h2>
-
-            <p>
-              İdeal Prefabrik üretimine ilk olarak prefabrike konutlar, ofis
-              binaları, deprem evleri ve hafif çelik yapılar ile başlamıştır…
-            </p>
-          </Col>
-          <Col lg={3}>
-            <h2>İLETİŞİM</h2>
-            <p>Türkiyenin Her Yerinden;</p>
-            <p className="number">
-              {" "}
-              <a href="tel:05541385012 ">
-                {" "}
-                <BsFillTelephoneFill /> 0554 138 50 12
-              </a>{" "}
-            </p>
-            <p>
-              <GrMail /> info@zirveprefabrilkonteyner.com
-            </p>
-          </Col>
-          <Col lg={3}>
-            <h2>ADRES</h2>
-
-            <p>Sanayi Mahallesi 60098 Nolu Cad No 36 Şehitkamil/Gaziantep</p>
-          </Col>
-          <Col lg={3}>
-            <h2>HAKKIMIZDA</h2>
-            <p>
-              ZİRVE PREFABRİK KONTEYNER, çok yönlü gelişen ve hızlı değişen
-              günümüz dünyasında, Prefabrik ve Konteyner Sektöründe yeniliklere
-              açık kişisel ve/veya kurumsal ihtiyaçlara cevap verebilen esnek
-              yapısı ile müşteri memnuniyeti odaklı bir üretim yapmaktadır.{" "}
-            </p>
-          </Col>
-          <a href={`https://wa.me/05541385012`} target="_blank">
-            <FaWhatsapp size={32} color="green" className="whatsUp" />
-          </a>
-        </Row>
-      </div> */}
+     
     </div>
   );
 };
